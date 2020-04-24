b5758951c331bfe698d98e14d1b3f0dedf631b7f
Garik <gvkhacha@uci.edu>
2020-04-24 15:02:37 -0700

    add content script

--- content.js
+++ content.js
@@ -1,1 +1,36 @@
-console.log("initial commit");
+console.log("starting");
+const parentElement = document.querySelector("div#content > article.post");
+
+if(parentElement == undefined){
+    chrome.runtime.sendMessage({
+        from: "content",
+        subject: "parentNotFound"
+    })
+}
+
+const bodyElements = [...parentElement.children];
+const removed = bodyElements.filter(child => !child.className.split(" ").includes("recipe"));
+
+removed.forEach(child => child.remove());
+
+chrome.runtime.sendMessage({
+    from: "content",
+    subject: "finishedRemoving",
+    count: removed.length
+})
+console.log("done");
+chrome.runtime.onMessage.addListener((req, sender) => {
+    console.log("received")
+    if(req.from === "popup"){
+        if(req.subject === "reAdd"){
+            console.log("Adding...")
+            const allChildren = removed.concat([...parentElement.children]);
+            console.log(allChildren);
+
+            parentElement.innerHTML = "";
+            allChildren.forEach(ch => parentElement.appendChild(ch));
+        }
+    }
+})
+
+
--- popup.js
+++ popup.js
@@ -1,5 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const introText = document.getElementById("introMessage");
 
-    introText.innerHTML = "Hello world";
+function updateText(count){
+    const introText = document.getElementById("introMessage");
+    introText.innerHTML = `Removed ${count} pieces of BS from this page. Click the button below to add them back.`;
+}
+
+function initText(){
+    updateText(0);
+}
+
+function initBtn(){
+    const reAddBtn = document.getElementById("reAddBtn");
+    reAddBtn.onclick = () => {
+        chrome.runtime.sendMessage({
+            from: "popup",
+            subject: "reAdd"
+        })
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    initText();
+    initBtn();
+
+    chrome.runtime.onMessage.addListener((req, sender) => {
+        if(req.from === "content"){
+            if(req.subject === "finishedRemoving"){
+                updateText(req.count);
+            }
+        }
+    })
 })
